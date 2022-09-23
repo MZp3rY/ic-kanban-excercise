4243b33a78d826c2a5af147f741d3db58f4bd6fe
MZp3rY <mzpery@gmail.com>
2022-09-23 19:27:13 +0200

    use mockData on Board

--- app/src/containers/Board/index.js
+++ app/src/containers/Board/index.js
@@ -1,28 +1,73 @@
 import React from 'react';
+import '@atlaskit/css-reset';
+import styled from 'styled-components';
+import { DragDropContext } from 'react-beautiful-dnd';
+
+const Container = styled.div`
+  display: flex;
+`;
 
 class Board extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      users: [],
+      users: {},
       usersUpdated: false,
+      tasks: {},
+      columns: {},
+      columnOrder: [],
+      boardUpdated: false,
     };
   }
 
-  componentDidUpdate() {
-    //  upload state.users from props.users {key=user.id => value=user.name }
-    let users = {};
-    for (const user of this.props.users) {
-      users[user.id] = user.name;
-    }
+  componentDidUpdate = () => {
     //  prevents infinite calls
     if(!this.state.usersUpdated) {
+      //  upload state.users from props.users {key=user.id => value=user.name }
+      let users = {};
+
+      for (const user of this.props.users) {
+        users[user.id] = user.name;
+      }
+
       this.setState(() => ({
         users: users,
         usersUpdated: true,
       }));
     }
-  }
+
+    if(!this.state.boardUpdated) {
+      //  upload state.users from props.users {key=user.id => value=user.name }
+      let tasks = {};
+      let columns = {};
+      let columnOrder = [];
+
+      for (const column of this.props.boardColumns) {
+        columns[column.id] = {
+          id: column.id,
+          title: column.name,
+          taskIds: [],
+        };
+        columnOrder.push(column.id);
+        for (const task of column.tasks) {
+          if( !columns[column.id].taskIds.includes(task.id) ) {
+            columns[column.id].taskIds.push(task.id);
+          }
+
+          if (!(task.id in tasks)) {
+            tasks[task.id] = task;
+          }
+        }
+      }
+
+      this.setState(() => ({
+        tasks: tasks,
+        columns: columns,
+        columnOrder: columnOrder,
+        boardUpdated: true,
+      }));
+    }
+  };
 
 
   render () {
@@ -30,19 +75,20 @@ class Board extends React.Component{
     if (typeof this.props.boardColumns === 'undefined' || typeof this.props.users === 'undefined')
       return;
 
-    console.log([this.props.boardColumns, this.props.users]);
-
-
     return (
       <>
         <div className="main-board">
-          <div className="row">
-            <h1>Board</h1>
-          </div>
+          <DragDropContext
+            onDragEnd = {this.onDragEnd}
+          >
+            <Container>
+              <h1>Board</h1>
+            </Container>
+          </DragDropContext>
         </div>
       </>
     );
   }
-};
+}
 
 export default Board;
